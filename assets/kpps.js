4b19d865d50f6ddd4d4c51df8906c80ed7ce7d2a
Andez Apriansyah <34587697+andezapriansyah@users.noreply.github.com>
2024-02-12 21:11:53 +0700

    kpps3

--- assets/kpps.js
+++ assets/kpps.js
@@ -39,3 +39,87 @@ function delDptP() {
     document.getElementById('valueDptP').innerHTML = result;
   }
 }
+
+//DPTb LAKI LAKI
+function clrDptbL() {
+  document.getElementById('valueDptbL').innerHTML = '0';
+}
+
+function addDptbL() {
+  var result = parseFloat(document.getElementById('valueDptbL').innerHTML) + 1;
+
+  document.getElementById('valueDptbL').innerHTML = result;
+}
+
+function delDptbL() {
+  var result = parseFloat(document.getElementById('valueDptbL').innerHTML) - 1;
+
+  if (result < 0) {
+    document.getElementById('valueDptbL').innerHTML = 0;
+  } else {
+    document.getElementById('valueDptbL').innerHTML = result;
+  }
+}
+
+//Dptb PEREMPUAN
+function clrDptbP() {
+  document.getElementById('valueDptbP').innerHTML = '0';
+}
+
+function addDptbP() {
+  var result = parseFloat(document.getElementById('valueDptbP').innerHTML) + 1;
+
+  document.getElementById('valueDptbP').innerHTML = result;
+}
+
+function delDptbP() {
+  var result = parseFloat(document.getElementById('valueDptbP').innerHTML) - 1;
+
+  if (result < 0) {
+    document.getElementById('valueDptbP').innerHTML = 0;
+  } else {
+    document.getElementById('valueDptbP').innerHTML = result;
+  }
+}
+
+//DPK LAKI LAKI
+function clrDpkL() {
+  document.getElementById('valueDpkL').innerHTML = '0';
+}
+
+function addDpkL() {
+  var result = parseFloat(document.getElementById('valueDpkL').innerHTML) + 1;
+
+  document.getElementById('valueDpkL').innerHTML = result;
+}
+
+function delDpkL() {
+  var result = parseFloat(document.getElementById('valueDpkL').innerHTML) - 1;
+
+  if (result < 0) {
+    document.getElementById('valueDpkL').innerHTML = 0;
+  } else {
+    document.getElementById('valueDpkL').innerHTML = result;
+  }
+}
+
+//DPK PEREMPUAN
+function clrDpkP() {
+  document.getElementById('valueDpkP').innerHTML = '0';
+}
+
+function addDpkP() {
+  var result = parseFloat(document.getElementById('valueDpkP').innerHTML) + 1;
+
+  document.getElementById('valueDpkP').innerHTML = result;
+}
+
+function delDpkP() {
+  var result = parseFloat(document.getElementById('valueDpkP').innerHTML) - 1;
+
+  if (result < 0) {
+    document.getElementById('valueDpkP').innerHTML = 0;
+  } else {
+    document.getElementById('valueDpkP').innerHTML = result;
+  }
+}
